0829da60028ba3f63905e278a3f16d99cd42e630
koji.kobayashi <kojikobayashi1840@gmail.com>
2018-05-21 23:03:28 +0900

    modify 2重のルーティング(Route in Route)

--- router_test/src/App.js
+++ router_test/src/App.js
@@ -9,36 +9,92 @@ const App = () => (
         <li><Link to="/about">About</Link></li>
         <li><Link to="/friends">Friends</Link></li>
       </ul>
-      <Route exact path='/' render={() => myComponent({ title: 'Home', contents: 'ようこそ' })} />
-      <Route path='/about' render={() => myComponent({ title: 'About', contents: '友達に投票するページです' })} />
-      <Route path='/friends' render={() => myComponent({ title: 'Friends', contents: '友達のリストです' })} />
+      <Route exact path='/' component={Home}></Route>
+      <Route path='/about' component={About}></Route>
+      <Route path='/friends' component={Friends}></Route>
     </div>
   </BrowserRouter>
 )
 
-const myComponent = (props) => {
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>ようこそ</p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>友人に投票するページです</p>
+  </div>
+);
+
+// 友達リスト
+const friendsList = [
+  {
+    id: 'serval',
+    nameJa: 'サーバル',
+    nameEn: 'Serval Cat',
+    family: 'ネコ目ネコ科ネコ属'
+  },
+  {
+    id: 'raccoon',
+    nameJa: 'アライグマ',
+    nameEn: 'Common raccoon',
+    family: 'ネコ目アライグマ科アライグマ属'
+  },
+  {
+    id: 'fennec',
+    nameJa: 'フェネック',
+    nameEn: 'Fennec',
+    family: 'ネコ目イヌ科キツネ属'
+  }
+];
+
+/**
+ * idにマッチする要素を取得
+ * @param {string} id
+ */
+const friendById = id => friendsList.find(item => item.id === id);
+
+const Friends = () => {
   return (
     <div>
-      <h2>{props.title}</h2>
-      <p>{props.contents}</p>
+      <h2>Friends</h2>
+      <Route exact path='/friends' component={FriendList} />
+      <Route path='/friends/:id' component={Friend}></Route>
     </div>
   );
-}
-// const Home = (props) => {
-// };
+};
 
-// const About = () => (
-//   <div>
-//     <h2>About</h2>
-//     <p>友人に投票するページです</p>
-//   </div>
-// );
+const FriendList = () => (
+  <ul>
+    {friendsList.map(friend => (
+      <li key={friend.id}>
+        <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
+      </li>
+    ))}
+  </ul>
+);
 
-// const Friends = () => (
-//   <div>
-//     <h2>Friends</h2>
-//     <p>フレンズのリスト</p>
-//   </div>
-// );
+const Friend = (props) => {
+  const { id } = props.match.params;
+  const friend = friendById(id);
+
+  if (friend === void 0) {
+    return (
+      <p>Friends with id '{id}' does not exist.</p>
+    );
+  }
+  const margin0 = { margin: 0 };
+  return (
+    <div style={{ border: '1px gray solid', display: 'inline-block', padding: 10 }}>
+      <p style={margin0}>{friend.family}</p>
+      <h2 style={margin0}>{friend.nameJa}</h2>
+      <p style={margin0}>{friend.nameEn}</p>
+    </div>
+  );
+};
 
 export default App;
